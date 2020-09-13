4c38f1bc671db026ccb7c833b47c61d22603e89d
jasondavindev <jason.davin12@gmail.com>
2020-09-13 15:27:17 -0300

    remove HAL pages

--- src/main/java/fatec/projects/app/blog/BlogApplication.java
+++ src/main/java/fatec/projects/app/blog/BlogApplication.java
@@ -2,8 +2,9 @@ package fatec.projects.app.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
 public class BlogApplication {
 
 	public static void main(String[] args) {
