2e3df6dd22a4470da57b8cd9dcf9e89f3b14c06d
toledompm <toledompm@gmail.com>
2020-09-14 21:45:32 -0300

    Create route to retrieve posts by id

--- backend/src/main/java/fatec/projects/app/blog/domain/post/controller/v1/PostController.java
+++ backend/src/main/java/fatec/projects/app/blog/domain/post/controller/v1/PostController.java
@@ -22,8 +22,13 @@ public class PostController {
         return PostWebConverter.convertFrom(postService.create(post));
     }
 
-    @GetMapping("/{userId}")
-    public List<PostResponse> retrievePosts(@PathVariable("userId") Long userId) {
+    @GetMapping("/user/{userId}")
+    public List<PostResponse> retrievePostsByUserId(@PathVariable("userId") Long userId) {
         return PostWebConverter.convertFrom(postService.retrievePostsOfUser(userId));
+    }
+
+    @GetMapping("/{postId}")
+    public PostResponse retrievePostsById(@PathVariable("postId") Long postId) {
+        return PostWebConverter.convertFrom(postService.findPost(postId));
     }
 }
