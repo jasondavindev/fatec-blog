0cf711cac1e84cdf4bc6cd7100f0aef44a02b622
toledompm <toledompm@gmail.com>
2020-09-14 22:55:25 -0300

    create tests for post service

---
+++ backend/src/test/java/fatecprojects/app/blog/domain/post/service/PostServiceTest.java
@@ -0,0 +1,35 @@
+package fatecprojects.app.blog.domain.post.service;
+
+
+import fatec.projects.app.blog.domain.post.entity.Post;
+import fatec.projects.app.blog.domain.post.repository.PostRepository;
+import fatec.projects.app.blog.domain.post.service.PostService;
+import fatecprojects.app.blog.mocks.MockCreation;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class PostServiceTest {
+    @InjectMocks
+    private PostService postService;
+
+    @Mock
+    private PostRepository postRepository;
+
+    @DisplayName("Create valid post")
+    @Test
+    void creation_success() {
+        Post post = MockCreation.postMock();
+        when(postRepository.save(any())).thenReturn(post);
+
+        Assertions.assertEquals(postService.create(post),post);
+    }
+}
